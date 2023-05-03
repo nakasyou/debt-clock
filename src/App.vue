d4b8efa7dd7c3c687741db43f3b302ee209ad10d
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-03 21:16:03 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h1>借金時計</h1>
+    {{ config.debt.start }}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import config from "../debt-clock.json" assert { type: "json" }
+
 export default defineComponent({
   
 })
