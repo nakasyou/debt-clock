8b1165da1588b4349098ff90bf4c1d04470d8035
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-04 09:20:22 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>借金時計</h1>
     {{ debt.changeFromMs }}
+    {{ debtNum }}
   </div>
 </template>
 
@@ -22,10 +23,18 @@ export default defineComponent({
           / 60 //毎秒
           / 1000 //毎ミリ秒
       },
+      debtNum: 0,
     }
   },
   methods: {
-  
+    update(){
+      const date = new Date()
+      const debt = (date.getTime() - this.debt.time) * this.debt.changeFromMs
+      this.debtNum = debt
+    }
+  },
+  mounted(){
+    setInterval(this.update)
   },
 })
 </script>
