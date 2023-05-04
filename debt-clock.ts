0a052c57854b0d5442f16e31bf412a73a6d167be
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-04 17:11:54 +0900

    Add population

--- debt-clock.ts
+++ debt-clock.ts
@@ -4,5 +4,9 @@ export default {
     "debtFromDay": 1068021300000000 - 1042589600000000,
     "day": 365,
     "debt": 1068021300000000
+  },
+  population: {
+    population: 1249407000,
+    view: "1億2494万7000人(2022)",
   }
 }
