8038ded5767d34457e634a04ab44e50f15453b25
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-05-04 17:33:47 +0900

    fix

--- src/components/PersonDebt.vue
+++ src/components/PersonDebt.vue
@@ -24,7 +24,7 @@ const debtsPre = (debts: number, population): number[] => {
   <div class="units">
     <div v-for="(unit, index) in debtsPre(debts,population)" :key="index">
       <span class="num">{{ unit }}</span>
-      <span class="unit">{{ "円万億兆"[debtsPre(debts).length - index] }}</span>
+      <span class="unit">{{ "円万億兆"[debtsPre(debts,population).length - index -1] }}</span>
     </div>
   </div>
 </template>
